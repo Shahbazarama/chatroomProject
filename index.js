c45b48da47cc6a7a94df7e5299153d96dd128f5d
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-07 11:12:46 -0700

    emit functionality

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
 let express = require('express');
 let app = express();
 let http = require('http').Server(app);
-let io = require('socket.io');
+let port = process.env.PORT || 80;
+let io = require('socket.io')(http);
 let path = require('path');
 
 app.use(express.static(__dirname + '/js'));
-
-const socket = io(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-socket.on('connection', (socket) => {
-  console.log('user.connected')
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 })
-
-socket.on('connection', (socket) => {
-  console.log('user.connected')
-
-  socket.on('disconnect', () => {
-    console.log('disconnected');
-  })
-})
-//
-// app.get('/', function (req, res) {
-//   //
-// })
-
-
-
-
-
-
-
-//////////////
-let port = process.env.PORT || 80;
 
 http.listen(port, function() {
   console.log('Listening on port', port);
-
-
 });
+
 module.exports = app
