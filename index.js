8a42199a5b3b91712243556c7c05f4270966cdde
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-07 11:41:52 -0700

    git fix

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-let express = require('express');
-let app = express();
-let http = require('http').Server(app);
-let port = process.env.PORT || 80;
-let io = require('socket.io')(http);
-let path = require('path');
-
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/style'));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-=======
 (function() {
   let express = require('express');
   let app = express();
