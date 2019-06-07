d92ae13145e4b59f9ab145cb1c42036ab03db7b9
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-07 10:28:57 -0700

    fixed

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
 let express = require('express');
 let app = express();
 let http = require('http').Server(app);
-let io = require('socket.io');
+let port = process.env.PORT || 8000;
+let io = require('socket.io')(http);
 let path = require('path');
 
 app.use(express.static(__dirname + '/js'));
 
-const socket = io(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-socket.on('connection', (socket) => {
+io.on('connection', function(socket){
   console.log('user.connected')
 })
-
-socket.on('connection', (socket) => {
-  console.log('user.connected')
-
-  socket.on('disconnect', () => {
-    console.log('disconnected');
-  })
-})
-//
-// app.get('/', function (req, res) {
-//   //
-// })
-
-
-
-
-
-
-
-//////////////
-let port = process.env.PORT || 8000;
 
 http.listen(port, function() {
   console.log('Listening on port', port);
-
-
 });
