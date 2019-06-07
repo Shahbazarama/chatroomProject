5dd6d9e0120937814082c526a0b67f047c5d02bb
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-07 13:38:01 -0700

    clean

--- js/chat.js
+++ js/chat.js
@@ -2,11 +2,11 @@ $(function () {
    var socket = io();
    $('#sendField').submit(function(e){
      e.preventDefault(); // prevents page reloading
-     socket.emit('chat message', $('#messageField').val());
+     socket.emit('chat message', $('#messageField').val(), $('#userField').val());
      $('#messageField').val('');
      return false;
    });
-   socket.on('chat message', function(message){
-     $('#messages').append($('<li>').text(message))
+   socket.on('chat message', function(message, username){
+     $('#messages').append($('<li>').text(`${username}: ${message}`))
    });
  });
