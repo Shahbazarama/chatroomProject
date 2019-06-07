260c024c21d96edb4c1f9532ac6cef1859f09b57
quincyshadow <inversenormal@gmail.com>
2019-06-07 09:33:20 -0700

    .

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+socket(function() {
+    var  socket  =  io();
+    $("form").submit(function(e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit("chat message", $("#m").val());
+        $("#m").val("");
+    return  true;
+});
+})();
